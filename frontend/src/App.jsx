e58a08351aeb72c9535d988d96c1bccc754cebb2
Manya537 <168168681+Manya537@users.noreply.github.com>
2026-04-14 00:15:28 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -26,7 +26,7 @@ import Search from './pages/Search'
 import getAllNotifications from './hooks/getAllNotifications'
 import Notifications from './pages/Notifications'
 import { setNotificationData } from './redux/userSlice'
-export const serverUrl="http://localhost:8000"
+export const serverUrl="https://vybe-backend-p17f.onrender.com"
 
 
 const App = () => {
